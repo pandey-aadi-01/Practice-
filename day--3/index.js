75e85b887c66abbd0d0d9b943f16945265b1227c
Your Name <your-email@example.com>
2026-02-12 23:39:51 +0530

    Initial commit

--- day--3/index.js
+++ day--3/index.js
@@ -65,3 +65,10 @@
 // const animals = ["lynx", "rabbit", "lynx", "wombat"];
 // let ans = animals.lastIndexOf("lynx");
 //
+// Return the sum of the values in the 'numbers' array using 'Array.reduce()'
+
+
+
+const numbers = [34, 12, 47, 112];
+const total = numbers.reduce((sum , numbers) => sum+numbers,0);
+return total;
--- day--4/script.js
+++ day--4/script.js
@@ -1,9 +1,3 @@
 
-// Return the sum of the values in the 'numbers' array using 'Array.reduce()'
 
-
-
-const numbers = [34, 12, 47, 112];
-const total = numbers.reduce((sum , numbers) => sum+numbers,0);
-return total;
 
